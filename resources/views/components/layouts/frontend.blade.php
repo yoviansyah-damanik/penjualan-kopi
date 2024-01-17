4a2d5acfa50a8152e7667b96744801a557dbf3e9
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2024-01-17 19:53:08 +0700

    update

--- resources/views/components/layouts/frontend.blade.php
+++ resources/views/components/layouts/frontend.blade.php
@@ -5,7 +5,6 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="csrf_token" value="{{ csrf_token() }}" />
     <link rel="icon" type="image/png" href="{{ Vite::image('logo.png') }}" />
     <title>{{ $title ?? config('app.name') }}</title>
     @vite(['resources/css/app.css', 'resources/js/app.js'])
