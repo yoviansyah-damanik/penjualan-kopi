851012b2e1216cfdfe5eb0657e6265b4b21185df
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2024-01-17 19:50:37 +0700

    update

--- resources/views/components/layouts/backend.blade.php
+++ resources/views/components/layouts/backend.blade.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta name="csrf_token" value="{{ csrf_token() }}" />
     <link rel="icon" type="image/png" href="{{ Vite::image('logo.png') }}" />
     <title>{{ $title ?? config('app.name') }} | {{ __('Dashboard') }}</title>
     <link rel="stylesheet" href="{{ asset('dashboard-assets/css/style.css') }}" />
