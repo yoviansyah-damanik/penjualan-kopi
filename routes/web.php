519239ec0bf9bf15e91744dfeb67303514aa7c6e
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2024-01-17 20:14:21 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('guest', 'caffeinated')
+Route::middleware('guest')
     ->group(function () {
         Route::get('/login', Login::class)
             ->name('login');
