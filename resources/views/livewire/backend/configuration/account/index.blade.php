41b4dd4dac2e7fc62077a4265f10f56c541a5265
Yoviansyah Rizki Pratama <yoviansyahrizkypratama@gmail.com>
2024-02-02 00:55:22 +0700

    Update index.blade.php

--- resources/views/livewire/backend/configuration/account/index.blade.php
+++ resources/views/livewire/backend/configuration/account/index.blade.php
@@ -1,8 +1,8 @@
 <div class="flex flex-col gap-4 px-4 lg:flex-row">
     <div class="flex-none w-full lg:w-1/3">
-        @livewire('backend\configuration\account.information')
+        <livewire:backend.configuration.account.information />
     </div>
     <div class="flex-none w-full lg:w-1/3">
-        @livewire('backend\configuration\account.password')
+        <livewire:backend.configuration.account.password />
     </div>
 </div>
